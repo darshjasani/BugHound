68552dde5c9745edbb001ece5b31d5353a5cd4b8
Darshkumar Jasani <jasanidarsh3@gmail.com>
2024-03-11 18:07:52 -0700

    File Added

--- src/Login.js
+++ src/Login.js
@@ -11,55 +11,73 @@ function Login() {
     
   return (
     <>
-        <div className='click'>
-            <button onClick={changeLogin}>Login</button>
-            <button onClick={changeSignUp}>Sign Up</button>
-        </div>
-        {display == 'login' ? (
-            <div className='login'>
-            <div>
-                Username :&nbsp;
-                <input type='text'/>
-            </div>
-            <div>
-                Password :&nbsp;
-                <input type='password' />
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </div>
-        ) : (
-            <div className='signup'>
-            <div>
-                Username :&nbsp;
-                <input type='text'/>
-            </div>
-            <div>
-                Password :&nbsp;
-                <input type='password' />
-            </div>
-            <div>
-                Confirm Password :&nbsp;
-                <input type='password' />
-            </div>
-            <div>
-                Role : &nbsp;
-                <select>
-                    <option>User</option>
-                    <option>Tester</option>
-                    <option>Manager</option>
-                    <option>Admin</option>
-                </select>
-            </div>
-            <div>
-                <button>Sign Up</button>
-            </div>
-        </div>
-        )}
-        
+        <div className='loginBody'>
+            <div className='card'>
 
-        
+                <div className='click'>
+                    <button 
+                    className='loginButton'
+                    onClick={changeLogin}
+                    style={{
+                        color:display=='login'?'black':'rgb(100,100,100)',
+                        borderLeft:display=='login'? '2px solid grey':'0px',
+                        borderTop:display=='login'? '2px solid grey':'0px'
+                    }}>Login</button>
+                    <button
+                    className='signupButton'
+                    onClick={changeSignUp}
+                    style={{
+                        color:display=='signup'?'black':'rgb(100,100,100)',
+                        borderRight:display=='signup'? '2px solid grey':'0px',
+                        borderTop:display=='signup'? '2px solid grey':'0px'}}>Sign Up</button>
+                </div>
+
+                <div className='detailsBody'>
+                    {display == 'login' ? (
+                        <div className='login'>
+                            <div>
+                                Username :<br/>
+                                <input type='text' placeholder='Enter username'/>
+                            </div>
+                            <div>
+                                Password :<br/>
+                                <input type='password' placeholder='Enter password'/>
+                            </div>
+                            <div>
+                                <button>Login</button>
+                            </div>
+                    </div>
+                    ) : (
+                        <div className='signup'>
+                        <div>
+                            Username :&nbsp;
+                            <input type='text' placeholder='Enter username'/>
+                        </div>
+                        <div>
+                            Password :&nbsp;
+                            <input type='password' placeholder='Enter password'/>
+                        </div>
+                        <div>
+                            Confirm Password :&nbsp;
+                            <input type='password' placeholder='Confirm password'/>
+                        </div>
+                        <div>
+                            Role : &nbsp;
+                            <select>
+                                <option>User</option>
+                                <option>Tester</option>
+                                <option>Manager</option>
+                                <option>Admin</option>
+                            </select>
+                        </div>
+                        <div>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
+                    )}
+                </div>
+            </div>
+        </div>
     </>
   )
 }
